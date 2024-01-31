3d47620b08a611c72eebb25e8b769c890ce4a83f
Taylan Santos <taylanmf@gmail.com>
2024-01-31 17:03:06 -0300

    Adicionando Categoria

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
-import './App.css';
-import Home from './pages/home/Home';
-import Navbar from './components/navbar/NavBar';
-import Footer from './components/footer/Footer';
-
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Footer from "./components/footer/Footer"
+import NavBar from "./components/navbar/NavBar"
+import Home from "./pages/home/Home"
+import DeletarCategoria from './components/categoria/deletarCategoria/DeletarCategoria'
+import FormularioCategoria from "./components/categoria/formCategoria/FormCategoria"
+import ListarCategorias from "./components/categoria/listarCategoria/ListarCategoria"
 function App() {
   return (
     <>
-    <Navbar/>
-    <Home/>
-    <Footer/>
+    <BrowserRouter>
+      <NavBar />
+      <div className="min-h-[80vh]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/categorias" element={<ListarCategorias />} />
+          <Route path="/cadastroCategoria" element={<FormularioCategoria />} />
+          <Route path="/editarCategoria/:id" element={<FormularioCategoria />} 
+          />
+          <Route path="/deletarCategoria/:id" element={<DeletarCategoria />} 
+          />  
+        </Routes>
+
+      </div>
+      <Footer />
+    </BrowserRouter>
     </>
-);
+  )
 }
-export default App;
+
+export default App
